6328d116e060946681f71c90062a568531927e26
jorge21ll <jorge.lllucio@gmail.com>
2022-02-21 23:41:20 -0300

    create types queries

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,21 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return this.repository.createQueryBuilder('users')
+    .leftJoinAndSelect('user.games', 'game')
+    .where('user.id = :id', { id: user_id })
+    .getOneOrFail(); // Complete usando ORM
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users WHERE LOWER(first_name) = $1 AND LOWER(last_name) = $2',
+    [first_name.toLowerCase(), last_name.toLowerCase()]); // Complete usando raw query
   }
 }
